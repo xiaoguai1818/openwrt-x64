757f1db80eb119f553c3c77f8669dc789ccca5dc
xiaoguai1818 <67789388+xiaoguai1818@users.noreply.github.com>
2021-07-15 11:42:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/UTC/Asia/Shanghai/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's#UTC#Asia/Shanghai#g' package/base-files/files/bin/config_generate
